7616ed2d0c276ed2398b407cd8183f17b3849411
prsion <prsion@mail.ru>
2021-09-03 23:50:25 +0300

    full

--- callAppBind.js
+++ callAppBind.js
@@ -4,16 +4,18 @@ return e + " My name is " +  this.name;
 
 var person = {
 name: "John",
-
+/*greet: greet*/
 };
 
 var anotherPerson = {
 name: "Doe",
-
+/*greet: greet*/
 };
 
 var bound= greet.bind(person );
 console.log(bound("Hello dude"));
 console.log(greet.call(person, 'Daroffa'));
-console.log(greet.apply(anotherPerson, ['Daroffa']));
+console.log(greet.apply(anotherPerson, ['Hiiiiiii']));
 
+/*console.log(anotherPerson.greet.call(person, 'Daroffa'));
+console.log(anotherPerson.greet.apply(anotherPerson, ['Hiiiiii']));*/
